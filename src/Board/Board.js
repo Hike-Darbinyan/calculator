560ae6367d39a642aa4928d1c6378cffc8ed5a10
Hike Darbinyan <hike19872@gmail.com>
2023-09-02 12:58:47 +0400

    calc-backgrounds

---
+++ src/Board/Board.js
@@ -0,0 +1,13 @@
+import './board.css'
+import Result from '../Result/Result';
+
+const Board = () =>
+{
+    return (
+        <div className="board">
+            <Result />
+        </div>
+    )
+}
+
+export default Board;
---
+++ src/Result/Result.js
@@ -0,0 +1,12 @@
+import './result.css';
+
+const Result = () =>
+{
+    return (
+        <div className='result-board'>
+            <div className='result'>12*12=144</div>
+        </div>
+    )
+}
+
+export default Result;
